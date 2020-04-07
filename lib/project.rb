542926189549971f8ae88600a171374e57fe9b9c
Javante Nelson <javantenelson@gmail.com>
2020-04-07 23:12:47 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) unless backer.back_project.include?(self)
+    backer.back_project(self) unless backer.backed_project.include?(self)
   end
   
 end
